44291e6cde182cae9d73503935db50f12ac9bbc3
Aestimo K <aestimo.njuguna@gmail.com>
2023-08-03 15:25:36 +0300

    Cleaning up

--- config/config.exs
+++ config/config.exs
@@ -2,8 +2,8 @@ import Config
 
 config :blog_app, Blog.Repo,
   database: "blog_app_repo",
-  username: "user",
-  password: "password",
+  username: "user", # change this
+  password: "password", # change this
   hostname: "localhost"
 
 config :blog_app, ecto_repos: [Blog.Repo]
